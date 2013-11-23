fcdb8cf3106958c349bcace65e6b33d70a7c17c1
Sam Moore <samandmoore@gmail.com>
2013-11-23 14:00:28 -0500

    use verbose flag for linking and don't link bin for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,25 @@
 
 DOTS="$PWD"
 
-ln -sF $DOTS/bash_profile ~/.bash_profile
-ln -sF $DOTS/bash_prompt ~/.bash_prompt
-ln -sF $DOTS/bashrc ~/.bashrc
-ln -sF $DOTS/aliases ~/.aliases
-ln -sF $DOTS/exports ~/.exports
-ln -sF $DOTS/functions ~/.functions
-ln -sF $DOTS/profile ~/.profile
+ln -sFv $DOTS/bash_profile ~/.bash_profile
+ln -sFv $DOTS/bash_prompt ~/.bash_prompt
+ln -sFv $DOTS/bashrc ~/.bashrc
+ln -sFv $DOTS/aliases ~/.aliases
+ln -sFv $DOTS/exports ~/.exports
+ln -sFv $DOTS/functions ~/.functions
+ln -sFv $DOTS/profile ~/.profile
 
-ln -sF $DOTS/vimrc ~/.vimrc
-ln -sF $DOTS/gvimrc ~/.gvimrc
+ln -sFv $DOTS/vimrc ~/.vimrc
+ln -sFv $DOTS/gvimrc ~/.gvimrc
 
-ln -sF $DOTS/gitconfig ~/.gitconfig
-ln -sF $DOTS/githelpers ~/.githelpers
-ln -sF $DOTS/hushlogin ~/.hushlogin
+ln -sFv $DOTS/gitconfig ~/.gitconfig
+ln -sFv $DOTS/githelpers ~/.githelpers
+ln -sFv $DOTS/hushlogin ~/.hushlogin
 
-ln -sF $DOTS/vim ~/.vim
-ln -sF $DOTS/bin ~/bin
+ln -sFv $DOTS/vim ~/.vim
+#ln -sFv $DOTS/bin ~/bin
+
+#BIN_FILES=$DOTS/bin/.*
+#for binfile in $BIN_FILES; do
+#    ln -sFv $DOTS/$binfile ~/bin/$binfile
+#done
