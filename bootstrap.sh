15fc299fb472aa1d533aee35c32460b58228f1c1
Sam Moore <samandmoore@gmail.com>
2013-11-23 13:31:58 -0500

    added linker

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+DOTS="$PWD"
+
+ln -sF $DOTS/bash_profile ~/.bash_profile
+ln -sF $DOTS/bash_prompt ~/.bash_prompt
+ln -sF $DOTS/bashrc ~/.bashrc
+ln -sF $DOTS/aliases ~/.aliases
+ln -sF $DOTS/exports ~/.exports
+ln -sF $DOTS/functions ~/.functions
+ln -sF $DOTS/profile ~/.profile
+
+ln -sF $DOTS/vimrc ~/.vimrc
+ln -sF $DOTS/gvimrc ~/.gvimrc
+
+ln -sF $DOTS/gitconfig ~/.gitconfig
+ln -sF $DOTS/githelpers ~/.githelpers
+ln -sF $DOTS/hushlogin ~/.hushlogin
+
+ln -sF $DOTS/vim ~/.vim
+ln -sF $DOTS/bin ~/bin
