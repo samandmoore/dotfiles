9068b9f3ac2db45f5a836040dcf37f5a74ca569f
Sam Moore <sam@betterment.com>
2024-09-10 22:33:05 -0400

    remove unused old style keymaps for new file creation

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,9 +7,3 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "Faster command entry with semicolon" })
 map("i", "jj", "<esc>", { desc = "Faster escape to normal mode" })
 map("n", "-", "<cmd>Neotree filesystem reveal current<cr>", { desc = "Open Neotree to current file in current window" })
-
--- create a new file and open in a new tab
--- http://vimcasts.org/e/14
-map("n", "<leader>ew", "<cmd>e <C-R>=expand('%:h').'/'<CR>", { desc = "New file in current window" })
-map("n", "<leader>es", "<cmd>sp <C-R>=expand('%:h').'/'<CR>", { desc = "New file in horizontal split" })
-map("n", "<leader>ev", "<cmd>vsp <C-R>=expand('%:h').'/'<CR>", { desc = "New file in vertical split" })
