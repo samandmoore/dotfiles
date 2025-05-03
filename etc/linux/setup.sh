c7657912f3c39e2f70a233966933585a8a30c2a8
Sam Moore <samandmoore@gmail.com>
2025-05-02 20:34:44 -0400

    setup linux scripts and break file up a bit

--- etc/linux/setup.sh
+++ etc/linux/setup.sh
@@ -1,133 +1,18 @@
-# better terminal emulator
-sudo apt install -y alacritty
-
-# Make alacritty default terminal emulator
-sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
-
-# Adding alacritty to nautilus contextual menu requires the python wrapper for the libraries
-sudo apt install -y python3-nautilus
-mkdir -p ~/.local/share/nautilus-python/extensions/
-
-cat >~/.local/share/nautilus-python/extensions/open-alacritty.py <<TECHNICALLYNOTACONFIGSOHEREDOCCEDITIS
-import os
-from urllib.parse import unquote
-from gi.repository import Nautilus, GObject
-from typing import List
-
-class OpenTerminalExtension(GObject.GObject, Nautilus.MenuProvider):
-    def _open_terminal(self, file: Nautilus.FileInfo) -> None:
-        filename = unquote(file.get_uri()[7:])
-
-        os.chdir(filename)
-        os.system("alacritty")
-
-    def menu_activate_cb(
-        self,
-        menu: Nautilus.MenuItem,
-        file: Nautilus.FileInfo,
-    ) -> None:
-        self._open_terminal(file)
-
-    def menu_background_activate_cb(
-        self,
-        menu: Nautilus.MenuItem,
-        file: Nautilus.FileInfo,
-    ) -> None:
-        self._open_terminal(file)
-
-    def get_file_items(
-        self,
-        files: List[Nautilus.FileInfo],
-    ) -> List[Nautilus.MenuItem]:
-        if len(files) != 1:
-            return []
-
-        file = files[0]
-        if not file.is_directory() or file.get_uri_scheme() != "file":
-            return []
-
-        item = Nautilus.MenuItem(
-            name="NautilusPython::openterminal_file_item",
-            label="Open in Alacritty",
-            tip="Open Alacritty In %s" % file.get_name(),
-        )
-        item.connect("activate", self.menu_activate_cb, file)
-
-        return [
-            item,
-        ]
-
-    def get_background_items(
-        self,
-        current_folder: Nautilus.FileInfo,
-    ) -> List[Nautilus.MenuItem]:
-        item = Nautilus.MenuItem(
-            name="NautilusPython::openterminal_file_item2",
-            label="Open in Alacritty",
-            tip="Open Alacritty In %s" % current_folder.get_name(),
-        )
-        item.connect("activate", self.menu_background_activate_cb, current_folder)
-
-        return [
-            item,
-        ]
-TECHNICALLYNOTACONFIGSOHEREDOCCEDITIS
-
-# github cli
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-  sudo apt update &&
-  sudo apt install gh -y
-
 # command line utilities
-sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
-
-# lazygit
-cd /tmp
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar -xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-rm lazygit.tar.gz lazygit
-mkdir -p ~/.config/lazygit/
-touch ~/.config/lazygit/config.yml
-cd -
-
-# Install mise for managing multiple versions of languages. See https://mise.jdx.dev/
-sudo apt update -y && sudo apt install -y gpg wget curl
-sudo install -dm 755 /etc/apt/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
-echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-sudo apt update
-sudo apt install -y mise
-
-# Gives you previews in the file manager when pressing space
-sudo apt install -y gnome-sushi
-
-# managing gnome settings
-sudo apt install -y gnome-tweak-tool
-
-# nicer terminal prompt
-sudo apt install starship
-
-# terminal multiplexer
-sudo apt install tmux
-
-# dotfiles helper
-sudo apt install rcm
-
-# signal
-wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |
-  sudo tee /etc/apt/sources.list.d/signal-xenial.list
-rm signal-desktop-keyring.gpg
-sudo apt update
-sudo apt install -y signal-desktop
-
-# Provides a system clipboard interface for Neovim under Wayland
-sudo apt install wl-clipboard
+sudo apt install -y \
+  fzf \
+  ripgrep \
+  bat \
+  eza \
+  zoxide \
+  plocate \
+  btop \
+  apache2-utils \
+  fd-find \
+  tldr \
+  rcm \
+  tmux \
+  starship
 
 # build libraries / tools
 sudo apt install -y \
@@ -135,3 +20,26 @@ sudo apt install -y \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
   libpq-dev postgresql-client postgresql-client-common
+
+source app-github-cli.sh
+source app-lazygit.sh
+source app-alacritty.sh
+source app-mise.sh
+
+# Provides a system clipboard interface for Neovim under Wayland
+sudo apt install wl-clipboard
+
+source fonts.sh
+
+# Gives you previews in the file manager when pressing space
+sudo apt install -y gnome-sushi
+
+# managing gnome settings
+sudo apt install -y gnome-tweak-tool
+
+source app-neovim.sh
+source app-signal.sh
+source app-vscode.sh
+source ulauncher.sh
+source set-gnome-extensions.sh
+source set-gnome-hotkeys.sh
