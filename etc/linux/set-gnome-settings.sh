9ba300624a9c73c49427a6efe255b9efcd106259
Sam Moore <samandmoore@gmail.com>
2025-05-08 13:19:57 -0400

    try out cascadia mono font for a bit

--- etc/linux/set-gnome-settings.sh
+++ etc/linux/set-gnome-settings.sh
@@ -1,2 +1,3 @@
 # Set Cascadia Mono as the default monospace font
-gsettings set org.gnome.desktop.interface monospace-font-name 'Intone Mono Nerd Font 10'
+# gsettings set org.gnome.desktop.interface monospace-font-name 'Intone Mono Nerd Font 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Caskaydia Mono Nerd Font 10'
