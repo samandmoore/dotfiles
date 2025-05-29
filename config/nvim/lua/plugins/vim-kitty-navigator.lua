a4fb35b5fda9b8cdbcb95dfdf4050fbcdf701877
Sam Moore <sam@betterment.com>
2025-05-29 15:47:19 -0400

    small cleanups and fix mason issue

--- config/nvim/lua/plugins/vim-kitty-navigator.lua
+++ config/nvim/lua/plugins/vim-kitty-navigator.lua
@@ -1,21 +1,18 @@
-if os.getenv("TERM") == "xterm-kitty" then
-  return {
-    "knubie/vim-kitty-navigator",
-    build = "cp ./*.py ~/.config/kitty/",
-    lazy = false,
-    cmd = {
-      "KittyNavigateLeft",
-      "KittyNavigateDown",
-      "KittyNavigateUp",
-      "KittyNavigateRight",
-    },
-    keys = {
-      { "<C-h>", "<cmd>KittyNavigateLeft<cr>" },
-      { "<C-j>", "<cmd>KittyNavigateDown<cr>" },
-      { "<C-k>", "<cmd>KittyNavigateUp<cr>" },
-      { "<C-l>", "<cmd>KittyNavigateRight<cr>" },
-    },
-  }
-else
-  return {}
-end
+return {
+  "knubie/vim-kitty-navigator",
+  build = "cp ./*.py ~/.config/kitty/",
+  lazy = false,
+  enabled = os.getenv("TERM") == "xterm-kitty",
+  cmd = {
+    "KittyNavigateLeft",
+    "KittyNavigateDown",
+    "KittyNavigateUp",
+    "KittyNavigateRight",
+  },
+  keys = {
+    { "<C-h>", "<cmd>KittyNavigateLeft<cr>" },
+    { "<C-j>", "<cmd>KittyNavigateDown<cr>" },
+    { "<C-k>", "<cmd>KittyNavigateUp<cr>" },
+    { "<C-l>", "<cmd>KittyNavigateRight<cr>" },
+  },
+}
