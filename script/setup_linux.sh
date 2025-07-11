c17b18868669d96e243f5229e5b7707fd81521fe
Sam Moore <samandmoore@gmail.com>
2025-07-10 20:50:59 -0400

    Update setup_linux.sh

--- script/setup_linux.sh
+++ script/setup_linux.sh
@@ -6,7 +6,7 @@ DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
 if [[ $DISTRO = 'ubuntu' ]]; then
   echo 'Ubuntu detected'
   source "$DOTFILES/etc/ubuntu/setup.sh"
-elif [[ $OS = 'arch' ]]; then
+elif [[ $DISTRO = 'arch' ]]; then
   echo 'Arch detected'
   source "$DOTFILES/etc/arch/setup.sh"
 else
