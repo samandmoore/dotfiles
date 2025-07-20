8839f4ed4c05b32301a96f539a3777f03c5f1abf
Sam Moore <samandmoore@gmail.com>
2025-07-19 20:24:09 -0400

    feat: firewall stuff

--- etc/arch/setup.sh
+++ etc/arch/setup.sh
@@ -101,6 +101,32 @@ paru -S --noconfirm --needed \
   1password-beta 1password-cli gnome-keyring \
   pinta
 
+# firewall stuff
+if ! command -v ufw &>/dev/null; then
+  paru -S --noconfirm --needed ufw ufw-docker
+
+  # Allow nothing in, everything out
+  sudo ufw default deny incoming
+  sudo ufw default allow outgoing
+
+  # Allow ports for LocalSend
+  sudo ufw allow 53317/udp
+  sudo ufw allow 53317/tcp
+
+  # Allow SSH in
+  sudo ufw allow 22/tcp
+
+  # Allow Docker containers to use DNS on host
+  sudo ufw allow in on docker0 to any port 53
+
+  # Turn on the firewall
+  sudo ufw enable
+
+  # Turn on Docker protections
+  sudo ufw-docker install
+  sudo ufw reload
+fi
+
 mkdir -p ~/.local/share/applications
 cp ~/.dotfiles/etc/arch/applications/*.desktop ~/.local/share/applications/
 cp ~/.dotfiles/etc/arch/applications/hidden/*.desktop ~/.local/share/applications/
