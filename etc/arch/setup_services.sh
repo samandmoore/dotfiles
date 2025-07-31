34a18ab3a5ee4a807cbfa8ab3c7af657ce540624
Sam Moore <samandmoore@gmail.com>
2025-07-30 20:46:45 -0400

    refactor: theme changer works now. it was kitty!

--- etc/arch/setup_services.sh
+++ etc/arch/setup_services.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 systemctl --user enable --now waybar.service
 systemctl --user enable --now mako.service
-systemctl --user enable --now walker.service
+# i have issues running walker as a service, so nope
+# systemctl --user enable --now walker.service
 systemctl --user enable --now swaybg.service
 systemctl --user enable --now swayosd.service
 systemctl --user enable --now polkit-gnome.service
