d739c9154328f045929904382d39bc18a8066a39
Sam Moore <samandmoore@gmail.com>
2025-07-31 21:30:20 -0400

    chore: clean up walker.service

--- etc/arch/setup_services.sh
+++ etc/arch/setup_services.sh
@@ -3,10 +3,7 @@ set -euo pipefail
 
 systemctl --user enable --now waybar.service
 systemctl --user enable --now mako.service
-# i have issues running walker as a service, so nope
-# systemctl --user enable --now walker.service
 systemctl --user enable --now swaybg.service
 systemctl --user enable --now swayosd.service
 systemctl --user enable --now polkit-gnome.service
 systemctl --user enable --now hypridle.service
-
