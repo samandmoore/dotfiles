cfbe0680d24ad921ba2966a3ffa6112644c32103
Sam Moore <samandmoore@gmail.com>
2025-08-05 15:45:08 -0400

    feat: add claude webapp

--- etc/arch/setup_applications.sh
+++ etc/arch/setup_applications.sh
@@ -8,3 +8,4 @@ update-desktop-database ~/.local/share/applications
 
 ~/.dotfiles/bin/arch-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 ~/.dotfiles/bin/arch-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+~/.dotfiles/bin/arch-webapp-install "Claude" https://claude.ai/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/claude-ai-light.png
