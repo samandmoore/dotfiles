fcbafd075befd7eb1886fdf07cc79d173149829e
Sam Moore <samandmoore@gmail.com>
2025-08-05 15:44:59 -0400

    refactor: move webapp installer to commands instead of bash functions
    never liked these in bash, this is nicer

--- etc/arch/setup_applications.sh
+++ etc/arch/setup_applications.sh
@@ -6,6 +6,5 @@ cp ~/.dotfiles/etc/arch/applications/*.desktop ~/.local/share/applications/
 cp ~/.dotfiles/etc/arch/applications/hidden/*.desktop ~/.local/share/applications/
 update-desktop-database ~/.local/share/applications
 
-source ~/.dotfiles/config/bash/functions
-web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
-web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+~/.dotfiles/bin/arch-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+~/.dotfiles/bin/arch-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
